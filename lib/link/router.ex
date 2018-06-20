dc1a34fb39aa94dd2426989aa39b5a301bb1846c
barabansheg <barabansheg@gmail.com>
2018-06-20 16:10:46 +0300

    add redirect and counter

--- lib/link/router.ex
+++ lib/link/router.ex
@@ -15,19 +15,35 @@ defmodule Link.Router do
     
     plug(:match)
     plug(:dispatch)
-    def do_new_link(conn) do
+    def add_link(conn) do
       %{"url" => url} = conn.body_params
       case UrlValidator.valid_url(url) do
         {:ok, valid_url} ->
           Mongo.insert_one(:mongo, "urls", %{url: valid_url, count: 0, hash: Randomizer.randomizer(6)}, pool: DBConnection.Poolboy)
           send_resp(conn, 200, "OK")
-        _ ->
+        _ ->  
           send_resp(conn, 422, "Validation Error")
       end
     end
+
+    def get_url_by_hash(hash) do
+      cursor =  Mongo.find(:mongo, "urls", %{hash: hash}, pool: DBConnection.Poolboy)
+      result = Enum.to_list(cursor)
+      [url_record | _] = result 
+      Mongo.update_one(:mongo, "urls", %{hash: hash}, %{"$inc": %{count: 1}}, pool: DBConnection.Poolboy)
+      url_record["url"]
+    end
+
+    def redirect_to_url(conn) do
+      url = get_url_by_hash(conn.path_params["hash"])
+      conn = Plug.Conn.put_resp_header(conn, "Location", url  ) 
+      send_resp(conn, 301, "Redirecting...")
+    end
+
     get("/", do: send_resp(conn, 200, "Welcome"))
     get("/ping", do: send_resp(conn, 200, "pong"))
-    post("/add", do: do_new_link(conn))
+    post("/add", do: add_link(conn))
+    get("/:hash", do: redirect_to_url(conn))
     match(_, do: send_resp(conn, 404, "Oops!"))
 
   end
